7c8ff8e9b78bff1e427e939b984fd03a188f613f
Kyle Leleux <kleleux@slac.stanford.edu>
2022-10-17 14:22:19 -0700

    Added next and previous buttons and getting in full LBLM list.

--- test_combobox.py
+++ test_combobox.py
@@ -1,57 +1,121 @@
-from os import path
-from PyQt5.QtWidgets import QComboBox
+import time
+import yaml
+import datetime
+import sys
+from os import path, listdir, environ
+from PyQt5.QtWidgets import QComboBox, QPushButton
 from epics import caget, caput
 from pydm import Display
+from mps_database.mps_config import MPSConfig, models, runtime
+from mps_database.tools.mps_names import MpsName
+from mps_database.tools.mps_reader import MpsDbReader
+from sqlalchemy import MetaData
 
 class ComboBoxMacroSelector(Display):
     def __init__(self, parent=None, args=[], macros=None):
         super(ComboBoxMacroSelector, self).__init__(parent=parent, args=args, macros=macros)
         
+        self.lblm_selection = 0
+
+        self.load_database()
+        self.get_lblms()
         #self.ui.PyDMTimePlot.clearCurves()
         self.init_comboBox()
         self.read_comboBox()
 #        self.write_macros()
         self.ui.comboBox.activated.connect(self.read_comboBox)
+        self.ui.Next.clicked.connect(self.next_lblm)
+        self.ui.Previous.clicked.connect(self.prev_lblm)
 
+    def load_database(self):
+        #first, find the path 
+        for file in listdir(environ.get("PHYSICS_TOP") + "/mps_configuration/current"):
+            if file.endswith(".db"):
+                self.db_file = environ.get("PHYSICS_TOP") + "/mps_configuration/current/" + file
+                print(self.db_file) 
+
+    def get_lblm_type(self):
+        #we want to query the database again, but this time we want all lblms
+        #because we want to note which ones are lblms and which are wire scanners
+        # lblms appear 4 times
+        # ws appear 2
+        # can use contains and make a list of these "types" 
+        
+        pass
+
+    def get_lblms(self):
+        
+        #reader = MPSConfig(self.db_file)
+        
+        #reader = MpsDbReader(self.db_file)
+        #dt = reader.session.query(models.DeviceType).filter(models.DeviceType.name=='BLM').one()
+        self.lblms = []
+        with MpsDbReader(self.db_file) as session:
+              dt = session.query(models.DeviceType).filter(models.DeviceType.name=='BLM').one()
+              print(dt)
+              devices = session.query(models.Device).filter(models.Device.device_type_id==dt.id).all()
+              for d in devices:
+                  if d.name.find('LBLM') > -1:
+                      self.lblms.append(d.name[4:])
+                      print(d.name)
+        #query the mps_database to get list of lblms
+
+    def sort_lblms(self):
+        #order by z position
+        pass
+    
     def init_comboBox(self):
         #ca://BLEN:HTR:350:10:RWF_U16.VALA
         #get this list from macros probably, if it is just the name, we will need to append :FAST_WF to the end.
-        lblm_list = ['-Select LBLM-', 'BLEN:HTR:350:10', 'ca://test2', 'ca://test3']
-        self.ui.comboBox.addItems(lblm_list)
+        #lblm_list = ['-Select LBLM-', 'BLEN:HTR:350:10', 'ca://test2', 'ca://test3']
+        self.ui.comboBox.addItems(self.lblms)
+        
+        #set previous button to disabled
+
 
     def read_comboBox(self):
-
+        #
         self.prev_text = self.macros()['DEVICE']
         self.macros()['DEVICE'] = self.ui.comboBox.currentText()
-        if self.macros()['DEVICE'] == '-Select LBLM-':
+        if self.macros()['DEVICE'] == '-Select LBLM-' or None:
             print('Not Writing')
         else:
             self.write_macros()
         
     def write_macros(self):
-        #self.ui.PyDMTimePlot.clearCurves() 
+        
         print('writing')
         print(self.macros()['DEVICE'])
-        #self.macros()['DEVICE'] = 'ca://' + self.text + ':RWF_U16.VALA'
         print(self.prev_text + ':RWF_U16.VALA') 
-        #curve = self.ui.PyDMTimePlot.findCurve(self.prev_text + ':RWF_U16.VALA')
-        #curve = self.ui.PyDMTimePlot.getCurves()
-        #self.ui.PyDMTimePlot.refreshCurve(curve)
-        #self.ui.PyDMTimePlot.set_needs_redraw()
-        #self.ui.PyDMTimePlot.redrawPlot()
-        #self.ui.PyDMTimePlot.addYChannel(self.text, plot_style='Line')
-        #print(self.ui.PyDMTimePlot.getCurves())
         self.ui.PyDMEmbeddedDisplay._needs_load = True
         self.ui.PyDMEmbeddedDisplay.load_if_needed()
-        #self.ui.PyDMTimePlot.createCurveItem(self.text, plot_style='Line')
-        #self.ui.PyDMTimePlot._curves.address = self.text
-        #print(self.ui.PyDMTimePlot.getCurves())
-        #print(self.ui.PyDMTimePlot.channel)
-        # when we write the macros, we need to remove the previous plot and add the new one.
-        # might need to do this by keeping track of the index. if they select the one that is 
-        # currently plotted, it ignores.
-        #self.ui.PyDMTimePlot.addYChannel(self.text)
-        #self.ui.PyDMTimePlot.channel = self.text
+
+    def next_lblm(self):
+        
+        lblm_index = self.ui.comboBox.currentIndex()
+        next_lblm = lblm_index + 1
+        if next_lblm < self.ui.comboBox.count():
+            self.ui.Next.setEnabled(True)
+            print(self.ui.comboBox.count())
+            print(next_lblm)
+            self.ui.comboBox.setCurrentIndex(next_lblm)
+            self.macros()['DEVICE'] = self.lblms[next_lblm]
+            self.write_macros()
+            if next_lblm == (self.ui.comboBox.count()-1):
+                self.ui.Next.setEnabled(False)
+
+    def prev_lblm(self):
+
+        lblm_index = self.ui.comboBox.currentIndex()
+        prev_lblm = lblm_index - 1
+        if (prev_lblm) >= 0:
+            print(lblm_index)
+            print(prev_lblm)
+            self.ui.comboBox.setCurrentIndex(prev_lblm)
+            self.macros()['DEVICE'] = self.lblms[prev_lblm]
+            self.write_macros()
+            if prev_lblm == 0:
+                self.ui.Previous.setEnabled(False)
 
     @staticmethod
     def ui_filename():
